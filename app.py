ccf89315c3b9b65405bd7f4dd70c8bbd8ec2c2e1
Drew White <d.white0002@gmail.com>
2023-02-23 09:17:46 -0800

    add flask boilerplate

--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 from flask import Flask, request, render_template
 import random
+
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/roll_dice', methods=['POST'])
+def roll_dice_route():
+    num_dice_input = request.form['num_dice']
+    num_dice = parse_input(num_dice_input)
+    roll_results = roll_dice(num_dice)
+    dice_face_diagram = generate_dice_faces_diagram(roll_results)
+    return render_template('roll_dice.html', dice_face_diagram=dice_face_diagram)
